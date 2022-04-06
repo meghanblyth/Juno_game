6856bc9dea735ba7ed06d88c13b324963bf37f73
Peter O <harmless_goose@icloud.com>
2022-04-06 16:51:13 +0100

    Added draw card functionality

--- src/App.js
+++ src/App.js
@@ -1,49 +1,65 @@
 import './App.css';
-import { Card } from './components/Card';
+import {useState} from 'react';
+//import { Card } from './components/Card';
 
 function App() {
 
   // Deck (random values)
   const drawCard = () => {
-    const number = Math.floor(Math.random() * 10);
+    const randomNumber = Math.floor(Math.random() * 10);
     const colourChoices = ['red', 'green', 'yellow', 'blue'];
-    const colour = colourChoices[Math.floor(Math.random()*colourChoices.length)];
+    const randomColour = colourChoices[Math.floor(Math.random() * colourChoices.length)];
 
     const randomCard = {
-      colour: colour,
-      number: number
+      colour: randomColour,
+      number: randomNumber
     };
 
     return randomCard;
   }
 
   // Hand (collection of cards)
-  let hand = [];
+  //const hand = [];
+  const [hand, setHand] = useState([]);
 
-  hand.push( drawCard() );
-  console.log(hand);
+  // Draw a card
+  const handleDrawCard = () => {
+    const newHand = hand.concat(drawCard());
+    setHand(newHand);
 
-  hand.push( drawCard() );
-  console.log(hand);
+    //hand.push(drawCard());
+    console.log(hand);
+  }
 
 
-    return (
+  return (
     <div className="App">
-
       <p>Juno!</p>
-
       <p>Hand:</p>
-      
-      <p> {
 
-        hand.map(
-          ({ colour, number }) => <li>{`Colour: ${colour} Number: ${number}` }</li>
-        )
-      }</p>
+      {/* Currently, the hand array isn't matching the hand output */}
+      <p>
+        {
+          hand.map(
+            ({ colour, number }) => <button> {
+              `Colour: ${colour} Number: ${number}`
+            } </button>
+          )
+        }
 
+        {/* {
+          hand.map(
+            ({ colour, number }) => <li> {
+              <Card colour={colour} number={number} />
+            } </li>
+          )
+        } */}
 
-      {/* Display hand of cards */}
-      <Card colour={drawCard().colour} number={drawCard().number}/>
+      </p>
+
+      <p>
+        <button onClick={handleDrawCard}>Draw a card</button>
+      </p>
 
     </div>
   );
