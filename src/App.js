4c8517f3de2f000e40e35cf37a50c07c21707ec2
Peter O <harmless_goose@icloud.com>
2022-04-06 15:23:19 +0100

    Made basic structure of Hand

--- src/App.js
+++ src/App.js
@@ -4,20 +4,46 @@ import { Card } from './components/Card';
 function App() {
 
   // Deck (random values)
-  const number = Math.floor(Math.random() * 10);
+  const drawCard = () => {
+    const number = Math.floor(Math.random() * 10);
+    const colourChoices = ['red', 'green', 'yellow', 'blue'];
+    const colour = colourChoices[Math.floor(Math.random()*colourChoices.length)];
 
-  const colourChoices = ['red', 'green', 'yellow', 'blue'];
+    const randomCard = {
+      colour: colour,
+      number: number
+    };
 
-  const colour = colourChoices[Math.floor(Math.random()*colourChoices.length)];
-  
+    return randomCard;
+  }
+
+  // Hand (collection of cards)
+  let hand = [];
+
+  hand.push( drawCard() );
+  console.log(hand);
+
+  hand.push( drawCard() );
+  console.log(hand);
+
 
     return (
     <div className="App">
 
       <p>Juno!</p>
 
-      {/* Cards */}
-      <Card colour={colour} number={number}/>
+      <p>Hand:</p>
+      
+      <p> {
+
+        hand.map(
+          ({ colour, number }) => <li>{`Colour: ${colour} Number: ${number}` }</li>
+        )
+      }</p>
+
+
+      {/* Display hand of cards */}
+      <Card colour={drawCard().colour} number={drawCard().number}/>
 
     </div>
   );
---
+++ src/components/Deck.js
@@ -0,0 +1,16 @@
+// export const Deck = () => {
+
+//   const number = Math.floor(Math.random() * 10);
+//   const colourChoices = ['red', 'green', 'yellow', 'blue'];
+//   const colour = colourChoices[Math.floor(Math.random() * colourChoices.length)];
+
+//   const randomCard = {
+//     colour: colour,
+//     number: number
+//   };
+
+//   return (
+//     { randomCard }
+//   );
+
+// }
