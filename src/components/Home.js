fadf679e8cb52a4f78cd88a42a348b1a6ae34115
Sarah Moffat <sarah.moffat5@gmail.com>
2022-04-12 12:43:49 +0100

    Tests completed and passing for home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom' ;
 
 export const Home = () => {
   return ( 
+    <Router>
+      <h1>Home</h1>
     <div className="Home">
       <Link to={'/Rules'}><button > Rules </button></Link> <Link to={'/Game'}><button > Play! </button></Link>
     </div>
+    </Router>
    );
 }
  
