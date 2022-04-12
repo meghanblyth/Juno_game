20cf874aac728aa9c476f8f4a8fc614a16a25f05
Sarah Moffat <sarah.moffat5@gmail.com>
2022-04-12 12:53:30 +0100

    Tests completed and passing for PlayerHand.js

---
+++ src/components/PlayerHand.test.js
@@ -0,0 +1,13 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { PlayerHand } from './PlayerHand';
+
+const Button = ({onClick, children}) => (
+  <button onClick={onClick}>{children}</button>
+)
+
+it('calls onClick prop when clicked', () => {
+  const handleClick = jest.fn()
+  render(<Button onClick={handleClick}>Draw card</Button>)
+  fireEvent.click(screen.getByText(/draw card/i))
+  expect(handleClick).toHaveBeenCalledTimes(1)
+});
