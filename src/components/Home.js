535117a5f2a9f1c778da4fb0bb5827f48acd4d42
Sarah Moffat <sarah.moffat5@gmail.com>
2022-04-13 10:28:06 +0100

    Router element removed from home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
-import { BrowserRouter as Router, Link } from 'react-router-dom' ;
+import { BrowserRouter as Link } from 'react-router-dom' ;
 
 export const Home = () => {
   return ( 
-    <Router>
-      <h1>Home</h1>
     <div className="Home">
+      <h1>Home</h1>
       <Link to={'/Rules'}><button > Rules </button></Link> <Link to={'/Game'}><button > Play! </button></Link>
     </div>
-    </Router>
    );
 }
  
